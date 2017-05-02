39caad2d554c69f3e621d834fb7c62b2357c956e
Stanley <snguyen414@gmail.com>
2017-05-02 00:40:25 -0500

    registration html and scss

--- itinerizer/public/js/script.js
+++ itinerizer/public/js/script.js
@@ -1,3 +1,31 @@
-$(function () {
- 
+// Wait for the DOM to be ready
+$(function() {
+  // Initialize form validation on the Sign In form.
+  // It has the name attribute "signIn"
+  $("form[name='signIn']").validate({
+    // Specify validation rules
+    rules: {
+      email: {
+        required: true,
+        email: true
+      },
+      password: {
+        required: true,
+        minlength: 5
+      }
+    },
+    // Specify validation error messages
+    messages: {
+      password: {
+        required: "Please provide a password",
+        minlength: "Your password must be at least 5 characters long"
+      },
+      email: "Please enter a valid email address"
+    },
+    // Make sure the form is submitted to the destination defined
+    // in the "action" attribute of the form when valid
+    submitHandler: function(form) {
+      form.submit();
+    }
+  });
 });
